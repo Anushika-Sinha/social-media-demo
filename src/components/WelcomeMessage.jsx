7cef4948bbb5268723ab2d457c350363b3b49183
Anushika Sinha <48572460+Anushika-Sinha@users.noreply.github.com>
2025-06-17 17:22:13 +0530

    Component Files

---
+++ src/components/WelcomeMessage.jsx
@@ -0,0 +1,9 @@
+const WelcomeMessage = ({ onGetPostsClick }) => {
+  return (
+    <center className="welcome-message">
+      <h1>No Posts Yet.</h1>
+    </center>
+  );
+};
+
+export default WelcomeMessage;
